5beeaa5e1acf63078bd79c2ce08299a06e758b48
FREDYK1 <fkwekukankam@gmail.com>
2024-09-28 22:44:32 +0000

    Sends emails when an event arises

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import selectorlib
-
+import ssl
+import smtplib
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
@@ -16,8 +17,38 @@ def extractor(source):
     value = selector.extract(source)["tours"]
     return value
 
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "frederickkankam7@gmail.com"
+    password = "wieu tuab eelc uubr"
+    receiver = "frederickkankam7@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host,port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message.encode('utf-8'))
+    print("Email was sent")
+
+
+def store(tour):
+    with open("Tours.txt", "a") as file:
+        file.write(tour + '\n')
+
+
+def read():
+    with open("Tours.txt", "r") as file:
+        return file.read()
+
+
+
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extractor(scraped)
+    Tours = read()
     print(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in Tours:
+            store(extracted)
+            send_email(extracted)
